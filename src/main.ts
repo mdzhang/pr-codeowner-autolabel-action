4bb82dcc8e3bc925521995a07f2afa6faf0c1ae2
meesh <meesh@found.com>
2024-01-18 12:43:34 -0500

    Initial implementation (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,134 @@
+/**
+ * Draws heavily from https://github.com/actions/labeler/blob/main/src/labeler.ts
+ */
+
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import * as pluginRetry from '@octokit/plugin-retry'
+import isEqual from 'lodash.isequal'
+import { minimatch } from 'minimatch'
+import * as api from './api'
+import { ClientType } from './types'
+
+// GitHub Issues cannot have more than 100 labels
+const GITHUB_MAX_LABELS = 100
 
 /**
  * The main function for the action.
+ *
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+export const run = async () =>
+  // eslint-disable-next-line github/no-then
+  labeler().catch(error => {
+    core.error(error)
+    core.setFailed(error.message)
+  })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+export const getInputs = () => ({
+  token: core.getInput('repo-token'),
+  filePath: core.getInput('file-path'),
+  labelsToOwner: core.getInput('labels-to-owners', { required: true }),
+  prNumber: github.context.payload.pull_request?.number
+})
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const flip = (data: Map<any, any>) =>
+  new Map(Object.entries(data).map(([key, value]) => [value, key]))
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+async function labeler() {
+  const { token, filePath, prNumber, labelsToOwner } = getInputs()
+
+  const client: ClientType = github.getOctokit(token, {}, pluginRetry.retry)
+
+  if (!prNumber) {
+    return
   }
+
+  const pullRequest = await api.getPullRequest(client, prNumber)
+  if (!pullRequest) {
+    return
+  }
+
+  const codeowners = await api.getCodeowners(client, prNumber, filePath)
+  if (codeowners.length === 0) {
+    return
+  }
+
+  core.debug(`labelsToOwner is ${labelsToOwner}`)
+  const labelMap: Map<any, any> = flip(JSON.parse(labelsToOwner))
+  core.debug(`labelMap is ${labelMap}`)
+  const preexistingLabels = pullRequest.data.labels.map(
+    (l: { name: string }) => l.name
+  )
+  const allLabels: Set<string> = new Set<string>(preexistingLabels)
+
+  const labels = getMatchingCodeownerLabels(
+    pullRequest.changedFiles,
+    codeowners,
+    labelMap
+  )
+  for (const label of labels) {
+    allLabels.add(label)
+  }
+  const labelsToAdd = [...allLabels].slice(0, GITHUB_MAX_LABELS)
+  let newLabels: string[] = []
+
+  try {
+    if (!isEqual(labelsToAdd, preexistingLabels)) {
+      await api.setLabels(client, pullRequest.number, labelsToAdd)
+      newLabels = labelsToAdd.filter(
+        label => !preexistingLabels.includes(label)
+      )
+    }
+  } catch (error: any) {
+    if (
+      error.name !== 'HttpError' ||
+      error.message !== 'Resource not accessible by integration'
+    ) {
+      throw error
+    }
+
+    core.warning(
+      `The action requires write permission to add labels to pull requests. For more information please refer to the action documentation: https://github.com/mdzhang/pr-codeowners-autolabeler-action#permissions`,
+      {
+        title: `${process.env['GITHUB_ACTION_REPOSITORY']} running under '${github.context.eventName}' is misconfigured`
+      }
+    )
+
+    core.setFailed(error.message)
+
+    return
+  }
+
+  core.setOutput('new-labels', newLabels.join(','))
+  core.setOutput('all-labels', labelsToAdd.join(','))
+}
+
+export function getMatchingCodeownerLabels(
+  changedFiles: string[],
+  entries: string[][],
+  labelMap: Map<string, string>
+): Set<string> {
+  // const repoUrlPrefix = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/blob`;
+  const allLabels: Set<string> = new Set<string>()
+
+  for (const changedFile of changedFiles) {
+    // const refPath = changedFile.blob_url.replace(repoUrlPrefix, '');
+    // const i = refPath.indexOf('/');
+    // const [_, path] = [refPath.slice(0,i), refPath.slice(i+1)];
+
+    core.debug(`checking path ${changedFile}`)
+    for (const entry of entries) {
+      const [glob, team] = entry
+      core.debug(`-- checking glob ${glob}, team ${team}`)
+      if (minimatch(changedFile, glob)) {
+        const label = labelMap.get(team)
+        if (label !== undefined) {
+          allLabels.add(label)
+        }
+      }
+    }
+  }
+
+  return allLabels
 }
