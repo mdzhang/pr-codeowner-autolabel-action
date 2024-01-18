e4dd333adeed3476eceb8790cfb2ce5596815f55
meesh <meesh@found.com>
2024-01-18 10:02:59 -0500

    scaffold

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,95 @@
-import * as core from '@actions/core'
+/**
+ * Draws heavily from https://github.com/actions/labeler/blob/main/src/labeler.ts
+ */
+
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as pluginRetry from '@octokit/plugin-retry';
+import isEqual from 'lodash.isequal';
+import * as api from './api';
+
+type ClientType = ReturnType<typeof github.getOctokit>;
+
+// GitHub Issues cannot have more than 100 labels
+const GITHUB_MAX_LABELS = 100;
 
 /**
  * The main function for the action.
  *
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+export const run = () =>
+  labeler().catch(error => {
+    core.error(error);
+    core.setFailed(error.message);
+  });
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+async function labeler() {
+  const { token, filePath, prNumbers, labelMap } = getInputs();
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    core.debug(new Date().toTimeString())
+  const client: ClientType = github.getOctokit(token, {}, pluginRetry.retry);
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+  const pullRequests = api.getPullRequests(client, prNumbers);
+
+  for await (const pullRequest of pullRequests) {
+    const codeowners: string[] = await api.getCodeownerEntries(client, filePath);
+    const preexistingLabels = pullRequest.data.labels.map(l => l.name);
+    const allLabels: Set<string> = new Set<string>(preexistingLabels);
+
+    const labels = getMatchingCodeownerLabels(pullRequest.changedFiles, codeowners, labelMap);
+
+    for (const label of labels) {
+      allLabels.add(label);
+    }
+
+    const labelsToAdd = [...allLabels].slice(0, GITHUB_MAX_LABELS);
+
+    let newLabels: string[] = [];
+
+    try {
+      if (!isEqual(labelsToAdd, preexistingLabels)) {
+        await api.setLabels(client, pullRequest.number, labelsToAdd);
+        newLabels = labelsToAdd.filter(
+          label => !preexistingLabels.includes(label)
+        );
+      }
+    } catch (error: any) {
+      if (
+        error.name !== 'HttpError' ||
+        error.message !== 'Resource not accessible by integration'
+      ) {
+        throw error;
+      }
+
+      core.warning(
+        `The action requires write permission to add labels to pull requests. For more information please refer to the action documentation: https://github.com/mdzhang/pr-codeowners-autolabeler-action#permissions`,
+        {
+          title: `${process.env['GITHUB_ACTION_REPOSITORY']} running under '${github.context.eventName}' is misconfigured`
+        }
+      );
+
+      core.setFailed(error.message);
+
+      return;
+    }
+
+    core.setOutput('new-labels', newLabels.join(','));
+    core.setOutput('all-labels', labelsToAdd.join(','));
   }
+}
+
+export function getMatchingCodeownerLabels(
+  changedFiles: string[],
+  entries: string[],
+  labelMap: Map<string, string>
+): string[] {
+  // TODO
+  for (const config of matchConfigs) {
+    core.debug(` checking config ${JSON.stringify(config)}`);
+    if (!checkMatch(changedFiles, config, dot)) {
+      return false;
+    }
+  }
+
+  return true;
 }
